8d0d48280d79690b862c4643c89a59fd790fbdc6
Rafael Serinolli <rafael.serinolli2@gmail.com>
2022-10-01 18:20:53 -0300

    Small changes when identifying

--- src/app/base/base.ts
+++ src/app/base/base.ts
@@ -1,4 +1,4 @@
-export abstract class Base{
+export abstract class SongStatusBase{
     isPlaying = false;
 
     constructor() { }
--- src/app/gettingInfo/begin/begin.component.ts
+++ src/app/gettingInfo/begin/begin.component.ts
@@ -7,13 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeginComponent implements OnInit {
 
+  nome: string | null = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    // if(localStorage.getItem('name') != null)
+    // {
+    //   this.nome = localStorage.getItem('name');
+    // }
+    
   }
 
-  submitName(name: string): void {
-    console.log(name);
+  submitName(p: string): void {
+    localStorage.setItem('name', p);
+    this.nome = p;
+  }
+
+  onClickNotMe() : void { 
+    this.nome = '';
+    // localStorage.removeItem('name');
+  }
+
+  onClickStart () : void {
+    console.log('App should start rn')
   }
 
 }
